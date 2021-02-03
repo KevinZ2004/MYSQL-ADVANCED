6abd4fc24e5eb578816bc01a3bbd316b7dcc725b
KevinZ2004 <70368913+KevinZ2004@users.noreply.github.com>
2021-02-03 12:58:11 +0100

    taak 02 - update 2 af

--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,8 +1,12 @@
 -- Opdracht 1
-
+UPDATE `studenten2` SET klas = '9X' WHERE student_id = 1
+UPDATE `studenten2` SET klas = '9X' WHERE student_id = 2
+UPDATE `studenten2` SET klas = '9X' WHERE student_id = 3
+UPDATE `studenten2` SET klas = '9X' WHERE student_id = 5
+UPDATE `studenten2` SET klas = '9X' WHERE student_id = 8
 -- Opdracht 2 
-
+UPDATE `studenten2` SET klas = '9Z' WHERE student_id>7 
 -- Opdracht 3
-
+UPDATE `studenten2` SET woonplaats = 'Amsterdam' WHERE student_id<=2 
 -- Opdracht 4
-
+UPDATE `studenten2` SET woonplaats = 'Uithoorn' WHERE klas = "9X" OR woonplaats = "Amsterdam" 
